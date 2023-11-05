592c115ee080592dc8d1c91c6dd853ee989b08e7
cczapatat <c.zapatat@uniandes.edu.co>
2023-11-04 23:02:22 -0500

    Brands totals

--- src/app/car/car-list/car-list.component.ts
+++ src/app/car/car-list/car-list.component.ts
@@ -10,13 +10,35 @@ import { CarService } from 'src/app/core/services/car.service';
 export class CarListComponent implements OnInit {
 
   public cars: Car[] = [];
+  public totalBrand: { brand: string, total: number }[] = [];
+
+  public isVisibleTotalBrand = false;
 
   constructor(private carService: CarService) { }
 
   getCars() {
     this.carService.getAll().subscribe((cars: Car[]) => {
       this.cars = cars;
+      this.calculateTotals();
     })
+  }
+
+  calculateTotals() {
+    const brands: Record<string, number> = {};
+
+    this.cars.forEach((car: Car) => {
+      if (!brands[car.marca]) {
+        brands[car.marca] = 1;
+      } else {
+        brands[car.marca] += 1;
+      }
+    });
+
+    Object.keys(brands).forEach(key => {
+      this.totalBrand.push({ brand: key, total: brands[key] });
+    });
+
+    this.isVisibleTotalBrand = this.totalBrand.length > 0;
   }
 
   ngOnInit() {
